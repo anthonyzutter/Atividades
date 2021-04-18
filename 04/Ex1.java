26ecf8b7b19f67dfeac9535d1de0b48f0ef621f4
anthonyzutter <60969070+anthonyzutter@users.noreply.github.com>
2021-04-18 19:06:09 -0300

    Enviou ex1

--- 04/Ex1.java
+++ 04/Ex1.java
@@ -1,1 +1,21 @@
+package ex;
 
+import java.time.LocalDate;
+
+public class Ex1 {
+    public static void main(String[] args) {
+        LocalDate data = LocalDate.now();
+        int anoAtual = data.getYear();
+        int anoInicio = 2015;
+        double salario = 1000;
+        double aumento = 1.5;
+
+        while (anoInicio < anoAtual) {
+            salario *= aumento / 100 + 1;
+            System.out.println("Aumento de " + aumento + "% Novo salário: R$" + salario);
+            aumento *= 2;
+            anoInicio++;
+        }
+        System.out.println();
+    }
+}
